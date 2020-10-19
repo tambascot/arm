6c5628f45e4abf67c67c29622ec5de40e34e9ab2
Tony Tambasco <tambascot@users.noreply.github.com>
2020-10-18 22:18:37 -0400

    Add java source file

---
+++ Arm.java
@@ -0,0 +1,37 @@
+package arm;
+
+import java.io.File;
+
+/**
+ * A.R.M. - Asst. Rentals Manager
+ * 
+ * A class to generate reports on logs and databases to make the life of the Gateway Rentals Manager 
+ *   / Asst. Production Manager easier. 
+ * 
+ * @author Tony Tambasco
+ * @author tambascot@yahoo.com
+ *
+ */
+
+public class Arm {
+	
+	private boolean updateLogReport(File logDirectory) {
+		
+		return true;
+	}
+	
+	private boolean upcomingRentalsReport(File rentalsDatabase) {
+		
+		return true;
+	}
+	
+	private boolean missingSoftgoodsReport(File softgoodsDatabase) {
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
