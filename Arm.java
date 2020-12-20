472abf9b078ac398ccc05d39c2c3a038b0aeba03
production <production@ProMgrAst.bellport.gatewayplayhouse.com>
2020-12-20 17:07:48 -0500

    Restoring to last working status, prior to inclusion of GMail features

--- Arm.java
+++ Arm.java
@@ -28,13 +28,8 @@ import org.apache.commons.cli.ParseException;
 import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-import org.w3c.dom.Element;
+//import org.w3c.dom.Element;
 import org.xml.sax.EntityResolver;
-
-// import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
-
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -47,16 +42,8 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Base64;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.gmail.Gmail;
-import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.Label;
-import com.google.api.services.gmail.model.ListLabelsResponse;
-import com.google.api.services.gmail.model.Message;
-import com.google.api.client.googleapis.services.json.*;
-//import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import com.google.api.services.sheets.v4.SheetsScopes;
 
-import javax.mail.Session;
-import javax.mail.internet.*;
 
 /**
  * A.R.M. - Asst. Rentals Manager
@@ -76,7 +63,7 @@ import javax.mail.internet.*;
 public class Arm {
 	
 	private static final String VERSION = "1.0.0.0";
-	private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_LABELS);
+	private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String APPLICATION_NAME = "A.R.M.";
 
@@ -86,6 +73,7 @@ public class Arm {
             return name.endsWith(".log");
         }
     };
+    
     
 	
 	@SuppressWarnings("deprecation")
@@ -106,16 +94,18 @@ public class Arm {
                 // Open and parse the file into an HOH
                 SAXBuilder parser = new SAXBuilder();
                 
+                
                 parser.setEntityResolver(new EntityResolver() {
-               	 @Override
-               	 public org.xml.sax.InputSource resolveEntity(String publicId, String systemId) throws IOException {
-               	  if (systemId.contains("logger.dtd")) {
-               	   return new org.xml.sax.InputSource(new StringReader(""));
-               	  } else {
-               	   return null;
-               	  }
-               	 }
-               	});
+                 	 @Override
+                  	 public org.xml.sax.InputSource resolveEntity(String publicId, String systemId) throws IOException {
+                  	  if (systemId.contains("logger.dtd")) {
+                  	   return new org.xml.sax.InputSource(new StringReader(""));
+                  	  } else {
+                  	   return null;
+                  	  }
+                  	 }
+                  	}) ;
+               	
                 
                 // Create a document given the URI to the file
                 Document doc = parser.build(logPath);
@@ -126,7 +116,7 @@ public class Arm {
                 java.util.List<org.jdom2.Element> records = root.getChildren("record");
                 
                 /* We need to make sure we're getting the records in order, and to do that we
-                 * need to create a sorted map, interate over the list of records, extract the sequence
+                 * need to create a sorted map, iterate over the list of records, extract the sequence
                  * number from each record, and then add the records to the sorted map using the 
                  * sequence number as a key. The first sequence should always be 0. If the current
                  * sequence is greater than the last sequence, replace last sequence with current
@@ -240,82 +230,7 @@ public class Arm {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
     
-    /**
-     * Create a MimeMessage using the parameters provided.
-     * copies from https://developers.google.com/gmail/api/guides/sending
-     *
-     * @param to email address of the receiver
-     * @param from email address of the sender, the mailbox account
-     * @param subject subject of the email
-     * @param bodyText body text of the email
-     * @return the MimeMessage to be used to send email
-     * @throws MessagingException
-     * @throws javax.mail.MessagingException 
-     * @throws AddressException 
-     */
-    public static MimeMessage createEmail(String to,
-                                          String from,
-                                          String subject,
-                                          String bodyText)
-            throws MessagingException, AddressException, javax.mail.MessagingException {
-        Properties props = new Properties();
-        Session session = Session.getDefaultInstance(props, null);
-
-        MimeMessage email = new MimeMessage(session);
-
-        email.setFrom(new InternetAddress(from));
-        email.addRecipient(javax.mail.Message.RecipientType.TO,
-                new InternetAddress(to));
-        email.setSubject(subject);
-        email.setText(bodyText);
-        return email;
-    }
-    
-    /**
-     * Create a message from an email.
-     * copied from https://developers.google.com/gmail/api/guides/sending
-     *
-     * @param emailContent Email to be set to raw of message
-     * @return a message containing a base64url encoded email
-     * @throws IOException
-     * @throws MessagingException
-     * @throws javax.mail.MessagingException 
-     */
-    public static Message createMessageWithEmail(MimeMessage emailContent)
-            throws MessagingException, IOException, javax.mail.MessagingException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        emailContent.writeTo(buffer);
-        byte[] bytes = buffer.toByteArray();
-        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
-        Message message = new Message();
-        message.setRaw(encodedEmail);
-        return message;
-    }
-    
-    /**
-     * Send an email from the user's mailbox to its recipient.
-     * copied from https://developers.google.com/gmail/api/guides/sending
-     *
-     * @param service Authorized Gmail API instance.
-     * @param userId User's email address. The special value "me"
-     * can be used to indicate the authenticated user.
-     * @param emailContent Email to be sent.
-     * @return The sent message
-     * @throws MessagingException
-     * @throws IOException
-     * @throws javax.mail.MessagingException 
-     */
-    public static Message sendMessage(Gmail service,
-                                      String userId,
-                                      MimeMessage emailContent)
-            throws MessagingException, IOException, javax.mail.MessagingException {
-        Message message = createMessageWithEmail(emailContent);
-        message = service.users().messages().send(userId, message).execute();
-
-        System.out.println("Message id: " + message.getId());
-        System.out.println(message.toPrettyString());
-        return message;
-    }
+  
 	
 	public static void main(String[] args) {
 		
@@ -446,16 +361,6 @@ public class Arm {
 	    	  String bodyText = "";
 	    	  
 	    	  try {
-	    		  final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		          Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT, jsonCredentialsFile, 
-		        		  tokensFilePath))
-		                  .setApplicationName(APPLICATION_NAME)
-		                  .build();
-		    	  updateLogReports.forEach((k, v) -> bodyText.concat(k + ": " + v + "\n"));
-	    		  MimeMessage message = createEmail(recipientAddress, "me", "Log Report", bodyText);
-	    		  Message emailMsg = createMessageWithEmail(message);
-	    		  sendMessage(service, "me", message);
-	    		  
 	    		  
 	    	  } catch (Exception e) {
 	    		  e.printStackTrace();
